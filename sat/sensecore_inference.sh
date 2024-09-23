a1fe47951b0942279d1301298be6360318f279e7
Jianlou <sijianlou@sensetime.com>
2024-09-23 12:44:19 +0800

    add sensecore run for infer and ft

---
+++ sat/sensecore_inference.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+HOME_DIR=${0%/*}
+cd ${HOME_DIR}
+HOME_DIR=$(pwd)
+EXP=${1:-cogvx-infer}
+MODEL_CONFIG=${2:-cogvideox_2b}
+RUN_CONFIG=${3:-inference_2b}
+DATETIME=$(date '+%Y-%m-%d-%H:%M:%S')
+environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
+
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo ${HOME_DIR}
+echo ${EXP}
+echo ${DATETIME}
+echo ${environs}
+
+srun --partition-id share-a \
+    --workspace-id d08f360b-7f9c-4eb1-bfb3-155fdad18726 \
+    --framework pt \
+    --job-name ${EXP} \
+    --resource N3lS.Ii.I60.1 \
+    --distributed StandAlone \
+    --output run_${DATETIME}.log \
+    --nodes 1 \
+    --priority NORMAL \
+    --container-image registry.cn-sh-01.sensecore.cn/devsft-ccr/ubuntu22.04_cuda12.4_cogvx:v1.0.3 \
+    --container-mounts 4ba8dc8e-52e5-11ee-82fd-de3a99f44f33:/mnt/afs_1 \
+    bash -c "cd "${HOME_DIR}"; source /root/miniconda3/bin/activate cogvx_sat; $environs python sample_video.py --base configs/${MODEL_CONFIG}.yaml configs/${RUN_CONFIG}.yaml --seed $RANDOM; sleep 1d"
+
+echo "DONE on `hostname`"
